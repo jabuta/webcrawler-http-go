c63824733efe4205454926ced382bc2a45aac92d
jabuta <lfalvaz@gmail.com>
2024-02-02 14:33:21 -0500

    Finished crawling fucntion, with recursive calls, added output sort (bubble)

--- main.go
+++ main.go
@@ -6,10 +6,35 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"golang.org/x/net/html"
 )
+
+type linkCount struct {
+	link  string
+	count int
+}
+
+func sortLinks(linkMap map[string]int) []linkCount {
+	linkCountList := make([]linkCount, len(linkMap))
+	k := 0
+	for link, count := range linkMap {
+		linkCountList[k] = linkCount{link, count}
+		k++
+	}
+	for i := 0; i < len(linkCountList)-1; i++ {
+		for j := i + 1; j < len(linkCountList); j++ {
+			if linkCountList[i].count < linkCountList[j].count {
+				interim := linkCountList[i]
+				linkCountList[i] = linkCountList[j]
+				linkCountList[j] = interim
+			}
+		}
+	}
+	return linkCountList
+}
 
 // Collect all links from response body and return it as an array of strings
 func getLinksOnPage(url string) ([]string, error) {
@@ -18,6 +43,7 @@ func getLinksOnPage(url string) ([]string, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
+
 	if res.StatusCode > 299 {
 		errBody, err := io.ReadAll(res.Body)
 		if err != nil {
@@ -39,6 +65,7 @@ func getLinksOnPage(url string) ([]string, error) {
 			if "a" == token.Data {
 				for _, attr := range token.Attr {
 					if attr.Key == "href" &&
+						len(attr.Val) > 0 &&
 						attr.Val[0:1] != "#" &&
 						!strings.Contains(attr.Val, "cdn-cgi") {
 						links = append(links, attr.Val)
@@ -49,40 +76,64 @@ func getLinksOnPage(url string) ([]string, error) {
 	}
 }
 
-func parseLinks(links []string, root string) []string {
-	var newLinks []string
+func normaliseLinks(links []string, root url.URL) []string {
+	var normLinks []string
 	for _, link := range links {
+		fmt.Printf("normalising %s\n", link)
 		fullLink := link
 		if link[0:1] == "/" {
-			fullLink = root + link
+			fullLink = root.Scheme + "://" + root.Host + link
 		}
 		u, err := url.Parse(fullLink)
-		if err != nil || u.Scheme == "" || u.Host == "" {
-			fmt.Printf("%v is not a proper URL\n", fullLink)
+		if err != nil || !strings.Contains(u.Scheme, "http") || u.Host == "" {
+			fmt.Printf("%v is not a proper http/s URL\n", fullLink)
 			continue
-		} else if u.Host != root {
+		} else if u.Host != root.Host {
 			fmt.Printf("%v is not an internal link\n", fullLink)
 			continue
 		}
-		newLinks = append(newLinks, u.Scheme+"://"+u.Host+u.Path)
+		normLinks = append(normLinks, "https://"+u.Host+u.Path)
 
 	}
-	return newLinks
+	return normLinks
+}
+
+func crawlSite(linksToCrawl []string, crawledLinks map[string]int, root url.URL) {
+	for _, link := range linksToCrawl {
+		if _, ok := crawledLinks[link]; ok {
+			crawledLinks[link]++
+			continue
+		}
+		if len(linksToCrawl) == 1 {
+			crawledLinks[link] = 0
+		} else {
+			crawledLinks[link] = 1
+		}
+		linksToParse, err := getLinksOnPage(link)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		parsedLinks := normaliseLinks(linksToParse, root)
+		crawlSite(parsedLinks, crawledLinks, root)
+	}
 }
 
 func main() {
+	crawledLinks := make(map[string]int)
 	fmt.Print("Enter Root Domain:")
-	rootURL := "https://www.obrasximpuestos.com"
-	linkToParse, err := getLinksOnPage(rootURL)
-	if err != nil {
+	rootURLString := os.Args[1]
+	rootURL, err := url.Parse(rootURLString)
+	if err != nil || rootURL.Scheme == "" || rootURL.Host == "" {
 		fmt.Println(err)
 		return
 	}
-	for _, link := range linkToParse {
-		fmt.Println(link)
+	linksToCrawl := normaliseLinks([]string{rootURLString}, *rootURL)
+	crawlSite(linksToCrawl, crawledLinks, *rootURL)
+
+	sortedLinks := sortLinks(crawledLinks)
+	for _, linkInfo := range sortedLinks {
+		fmt.Printf("%s has %v instances\n", linkInfo.link, linkInfo.count)
 	}
-	parsedLinks := parseLinks(linkToParse, rootURL)
-	for _, link := range parsedLinks {
-		fmt.Println(link)
-	}
+
 }
